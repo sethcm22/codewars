9a830a116c5b68c641b06a6a3fb2c030e88d3978
Coty Seth <sethcm22@gmail.com>
2023-02-16 13:24:27 -0500

    completed Sqare(n) Sum

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function nextId(ids) {
 }
 // Valid but try using .includes() in next attempt.
 //***********
+// Square(n) Sum https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+function squareSum(numbers) {
+  const sum = numbers.reduce(
+    (total, currentVal) => total + currentVal * currentVal,
+    0
+  );
+  return sum;
+}
+// **********
